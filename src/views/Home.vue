6cd301003cf2feb7191b2ab50a36b9f60e677cfb
iiEnchant <nawalmaeruf2@gmail.com>
2022-02-15 10:58:07 +0000

    homepage+droplist, specifically App.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="cardiomessage">
+      <HelloWorld msg="cardio" />
+    </div>
+    <img class="heart" alt="Cardiac" src="../assets/CardianTransparent2.png" />
+    <img
+      class="heartsmall"
+      alt="Cardiac"
+      src="../assets/CardianTransparent2.png"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+
+<style>
+.heart {
+  position: relative;
+  top: -47px;
+  right: -45px;
+  float: right;
+  height: 25%;
+  width: 25%;
+  transform: scaleX(-1);
+}
+
+.heartsmall {
+  position: relative;
+  top: -47px;
+  right: -185px;
+  float: right;
+  height: 15%;
+  width: 15%;
+}
+
+.cardiomessage {
+  height: 75%;
+  width: 100%;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+}
+</style>
