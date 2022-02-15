8b55a6486efd445ed7843220e6e9e2446e36fd9c
Tom Misson <11tmisson@gmail.com>
2022-02-15 16:15:21 +0000

    Return JSON for news feed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,36 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <p>{{articles}}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { getArticles }  from '../api/newsFeedApi';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      loading: false,
+      articles: []
+    }
+  },
+  created() {
+    this.getDataFromApi()
+  },
+  methods: {
+    async getDataFromApi() {
+        this.loading = true;
+        this.articles = await getArticles();
+        this.loading = false;
+    }
   }
 }
 </script>
