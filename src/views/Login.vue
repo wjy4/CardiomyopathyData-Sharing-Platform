2f1a70fd6a1ca2ce7d7fc444a83636df4e1b6eb7
wjy4 <W1097304549@gmail.com>
2022-02-16 02:00:54 +0000

    Update Login.vue
    add rest password function page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="login">
     <h1>This will be the login page</h1>
+     <p> Forgot your Password ?
+     <router-link to="/RestPassword">Rest Password</router-link> </p>
   </div>
 </template>
