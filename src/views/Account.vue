2d30878a3007d7005621e8d45309bba4b6712715
wjy4 <W1097304549@gmail.com>
2022-02-20 00:25:27 +0000

    Update Account.vue

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="account">
-   <!-- <h1>Make sure you're logged in to view your account</h1> !-->
     <h1> Welcome back { { username } }</h1>
+    <div class="flex-parent">
+  <div class="account-info">
+     <h2>Name :{ { username } }</h2>
+    </div>
+  <div class="account-info">
+   <h2> Account Type : { { accountype } }</h2>
+    </div>
+    <div class="account-info">
+  <h2>  Email address : { { email } }</h2>
+    </div>
+    <div class="account-info">
+   <h2> Phone number : { { phonenumber } }</h2>
+    </div>
+    <div class="account-info">
+   <h2> Address : { { user-adress } }</h2>
+    </div>
+</div>
+<div class = "submit-parent">
+  <div class = "submit">
+  <input type = "submit" value="Submited Data">
   </div>
-  <div class = "account-info">
-    <h4> Name : { { username } }</h4>
-    <h4> Account Type : { { accountype } }</h4>
-    <h4> Email address : { { email } }</h4>
-    <h4> Phone number : { { phonenumber } }</h4>
-    <h4> Address : { { user-adress } }</h4>
-  </div>
-    <input type = "submit" class = "li" value="Submited Data">
+</div>
+
 </template>
 
 
@@ -25,11 +37,40 @@ h1{
   margin-top: 90px;
   margin: auto;
 }
-.li{
-  margin-left: 50%;
+
+
+
+.flex-parent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.account-info {
+  background-color: lightgray;
+   width: 800px;
+  border: 15px solid white;
+  padding: 80px;
+  height: 400px;
+  margin-top: 100px; 
+  
+}
+h2{
+  font-size: 20px;
+}
+.submit-parent{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.submit{
+  background-color: lightgray;
+   width: 400px;
+  border: 15px solid white;
+  padding: 80px;
+  height: 250px;
+  margin: center;
   font-size: 30px;
-  margin-top: 90px;
-  margin: auto;
 }
 
 </style>
