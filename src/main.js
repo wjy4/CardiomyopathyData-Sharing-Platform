167a292043e6614ab7aa5c6456ecfcdb1e2fade1
wjy4 <W1097304549@gmail.com>
2022-02-21 09:45:14 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,17 +3,6 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import App from './App.vue'
 import router from './router'
-import firebase from 'firebase'
-
-const firebaseConfig = {
-    apiKey: "AIzaSyB86JL8qR8tG-KMiMHuCuVN5VgkyZfepKw",
-    authDomain: "advancedwebassignment.firebaseapp.com",
-    projectId: "advancedwebassignment",
-    storageBucket: "advancedwebassignment.appspot.com",
-    messagingSenderId: "451838322437",
-    appId: "1:451838322437:web:819a6ba925591c8ac8396c"
-  };
-  firebase.initializaApp(firebaseConfig);
 
 
 const app = createApp(App);
