eb58e6de425092028da1240046640f2da7f04e66
tomMisson <11tmisson@gmail.com>
2022-02-21 09:57:15 +0000

    Fix build

--- src/router/index.js
+++ src/router/index.js
@@ -47,15 +47,6 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/Register.vue"),
   },
   {
-    path: "/dashboard",
-    name: "Dashboard",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Dashboard.vue"),
-  },
-  {
     path: "/:catchAll(.*)",
     name: "404",
 
