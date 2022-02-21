d66b6be8c5dbb6f51bb3396430032ffb71ee8f65
wjy4 <W1097304549@gmail.com>
2022-02-21 09:44:38 +0000

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="login">
-    <h1>This will be the login page</h1>
      <p> Forgot your Password ?
      <router-link to="/RestPassword">Rest Password</router-link> </p>
   </div>
